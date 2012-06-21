338806f94556998331c489eb8de4488c5a552f03
Kiril Angov <kiril.angov@gmail.com>
2012-06-21 20:52:27 +0300

    Adding link_to_frontend() helper function

--- lib/helper/iceBackendHelper.php
+++ lib/helper/iceBackendHelper.php
@@ -1,6 +1,27 @@
 <?php
 
+function link_to_frontend($name, $routeName, $params, $options = array())
+{
+  return link_to($name, url_for_frontend($routeName, $params), $options);
+}
+
+function url_for_frontend($name, $parameters)
+{
+  /** @var $application backendConfiguration */
+  $application = sfProjectConfiguration::getActive();
+
+  return $application->generateFrontendUrl($name, $parameters);
+}
+
+/**
+ * @depricated
+ *
+ * @param $name
+ * @param $parameters
+ *
+ * @return mixed
+ */
 function url_to_frontend($name, $parameters)
 {
-  return sfProjectConfiguration::getActive()->generateFrontendUrl($name, $parameters);
+  return url_for_frontend($name, $parameters);
 }
