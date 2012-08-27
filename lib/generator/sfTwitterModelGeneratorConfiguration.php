90ee3e0c2b30acba9b6b225ba23f5e6a19d5e348
Kiril Angov <kiril.angov@gmail.com>
2012-08-28 04:32:56 +0900

    Adding sfTwitterBootstrapPlugin as part of IceBackendPlugin and later will extract functionality from it into the main theme

---
+++ lib/generator/sfTwitterModelGeneratorConfiguration.php
@@ -0,0 +1,24 @@
+<?php
+
+abstract class sfTwitterModelGeneratorConfiguration extends IceModelGeneratorConfiguration
+{
+  /**
+   * Gets a new form object.
+   *
+   * @param  mixed $object
+   * @param  array $options An array of options to merge with the options returned by getFormOptions()
+   *
+   * @return sfForm
+   */
+  public function getForm($object = null, $options = array())
+  {
+    $class = $this->getFormClass();
+    $formObject = new $class($object, array_merge($this->getFormOptions(), $options));
+
+    $formatterObj = $formObject->getWidgetSchema()->getFormFormatter();
+    $formatterObj->setValidatorSchema($formObject->getValidatorSchema());
+
+    return $formObject;
+  }
+
+}
