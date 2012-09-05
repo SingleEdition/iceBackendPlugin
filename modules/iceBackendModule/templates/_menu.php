6c0a9a9936a189a53bfa079effb50a16fcec0ef9
Kiril Angov <kiril.angov@gmail.com>
2012-09-05 14:53:37 +0900

    Upgrading bootstrap less files to version 2.1.0

--- modules/iceBackendModule/templates/_menu.php
+++ modules/iceBackendModule/templates/_menu.php
@@ -5,7 +5,7 @@
  */
 ?>
 
-<div class="navbar navbar-fixed-top">
+<div class="navbar navbar-inverse navbar-fixed-top">
   <div class="navbar-inner">
     <div class="container-fluid">
       <a class="brand" href='<?php echo url_for('homepage') ?>'>
